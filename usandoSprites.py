6c5da138c7f1b4eef52ca30eb5c65a3360e02de8
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-20 09:22:19 -0500

    Agrego comentarios para explicar el código

--- usandoSprites.py
+++ usandoSprites.py
@@ -18,18 +18,27 @@ H_50D2FE = (94, 210, 244)
 
 class Jugador (pygame.sprite.Sprite):
     # Sprite del jugador
+    """ Hereda métodos y atributos de la clase Sprite de Pygame
+    Sprite es una clase usada para representar objetos en un juego, tales
+    como personakes, enemigos, obstáculos, etc"""
+
     def __int__(self):
         # Heredo el init de la clase Sprite de Pygame
         super().__init__()
 
-        # Rectángulo (jugador)
+        """ Creo una superficie (surface) de color rojo oscuro (definido como
+        una constante previamente), que es el color de fondo de la imagen 
+        del jugador."""
         self.image = pygame.Surface((200, 200))
         self.image.fill(H_FA2F2F)
 
-        #Obtiene el rectángulo (sprite)
+        """ Obtengo el rectángulo que rodea a la superficie del jugador, 
+        utilizando el método get_rect() de la superficie. 
+        El rectángulo se utiliza para detectar colisiones entre el jugador 
+        y otros objetos en el juego."""
         self.rect = self.image.get_rect()
 
-        # Centra el rectángulo (sprite)
+        # Centro el rectángulo (jugador)
         self.rect.center = (ANCHO // 2, ALTO // 2)
 
     def update(self):
