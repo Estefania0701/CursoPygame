992f2c340039ee46d46400eb6c5027c8614dc80e
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-20 11:15:07 -0500

    Corrijo errores de sintaxis, agrego más comentarios y finalizo el código

--- usandoSprites.py
+++ usandoSprites.py
@@ -16,13 +16,15 @@ VERDE = (0, 255, 0)
 AZUL = (0, 0, 255)
 H_50D2FE = (94, 210, 244)
 
-class Jugador (pygame.sprite.Sprite):
-    # Sprite del jugador
+
+class Jugador(pygame.sprite.Sprite):
     """ Hereda métodos y atributos de la clase Sprite de Pygame
     Sprite es una clase usada para representar objetos en un juego, tales
     como personakes, enemigos, obstáculos, etc"""
 
-    def __int__(self):
+    def __init__(self):
+        # Método constructor
+
         # Heredo el init de la clase Sprite de Pygame
         super().__init__()
 
@@ -33,17 +35,19 @@ class Jugador (pygame.sprite.Sprite):
         self.image.fill(H_FA2F2F)
 
         """ Obtengo el rectángulo que rodea a la superficie del jugador, 
-        utilizando el método get_rect() de la superficie. 
-        El rectángulo se utiliza para detectar colisiones entre el jugador 
-        y otros objetos en el juego."""
+        utilizando el método get_rect() de la superficie. El rectángulo 
+        se utiliza para detectar colisiones entre el jugador y otros objetos 
+        en el juego."""
         self.rect = self.image.get_rect()
 
         # Centro el rectángulo (jugador)
         # mediante división para que cambie si modifico ANCHO y ALTO después
-        self.rect.center = (ANCHO // 2, ALTO // 2) # (300, 400)
+        self.rect.center = (ANCHO // 2, ALTO // 2)  # (300, 400)
 
     def update(self):
-        # Actualiza esto cada vuelta de bucle
+        """ Actualiza el estado de un sprite en cada ciclo del bucle principal
+        del juego. Se puede sobrescribir para controlar el movimiento y la
+        interacción del jugador con otros objetos en el juego"""
 
         """ Actualizo la posición del rectángulo del jugador en el eje y,
         moviéndolo hacia abajo en la ventana del juego."""
@@ -52,4 +56,71 @@ class Jugador (pygame.sprite.Sprite):
         """ Verifico si el borde superior del rectángulo del jugador ha 
         pasado el borde inferior de la ventana del juego"""
         if self.rect.top > ALTO:
-            self.rect.bottom = 0 # Su posición se restablece en la parte superior de la ventana
+            self.rect.bottom = 0  # Su posición se restablece en la parte superior de la ventana
+
+
+# ------------------------------------------------------------------------
+
+# Inicializo todos los módulos de Pygame
+pygame.init()
+
+# Creo una ventana para mostrar el juego, asignándole dimensiones
+pantalla = pygame.display.set_mode((ANCHO, ALTO))
+
+# Establezco un título para la ventana
+pygame.display.set_caption("Trabajando con Sprites")
+
+""" Creo un objeto Clock de Pygame que se utiliza para controlar la 
+velocidad de cuadros del juego. Este objeto se utiliza en el bucle 
+principal del juego para asegurarse de que el juego se ejecute a una 
+velocidad constante y no se ralentice en exceso."""
+clock = pygame.time.Clock()
+
+# --------------------------- GRUPO DE SPRITES -------------------------------
+
+"""Creo un grupo de sprites vacíos. Los grupos de sprites se utilizan 
+para manejar y actualizar varios sprites simultáneamente"""
+sprites = pygame.sprite.Group()
+
+# Creo un objeto de tipo Jugador
+jugador = Jugador()
+
+"""Agrego el objeto jugador al grupo de sprites. Esto permite que el objeto 
+jugador sea actualizado y dibujado en la ventana del juego junto con otros 
+sprites que puedan estar presentes en el grupo sprites"""
+sprites.add(jugador)
+
+# ----------------------- BUCLE PRINCIPAL DEL JUEGO ---------------------------
+
+ejecutando = True
+while ejecutando:
+
+    """ Limito la velocidad de fotogramas del juego al valor definido
+    en la constante FPS"""
+    clock.tick(FPS)
+
+    # Para cerrar la ventana y detener el juego
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            ejecutando = False
+
+    # Actualizo el estado de cada sprite en el juego
+    sprites.update()
+
+    # Relleno la pantalla de color negro
+    pantalla.fill(NEGRO)
+
+    # Dibujo los sprites en la pantalla, con el método draw del grupo de sprites
+    sprites.draw(pantalla)
+
+    # Dibujo 2 líneas para crear un plano cartesiano
+    pygame.draw.line(pantalla, H_50D2FE, (400, 0), (400, 800), 1)
+    pygame.draw.line(pantalla, AZUL, (0, 300), (800, 300), 1)
+
+    # Actualizo la ventana con los cambios realizados
+    pygame.display.flip()
+
+# Cierro todos los módulos Pygame que había inicializado antes
+""" NOTA: Si no se llama a pygame.quit(), se pueden producir errores o 
+conflictos en el sistema y el programa puede no cerrarse adecuadamente."""
+pygame.quit()
